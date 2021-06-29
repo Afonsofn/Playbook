aa0ab8624dbdcac7cfa68fa0fe94d7a2b0f927cb
Afonso <69821028+Afonsofn@users.noreply.github.com>
2021-06-29 09:48:46 -0300

    Update matando-setTimeOut-para-requisicoes-em-inputs.js

--- Lógica/matando-setTimeOut-para-requisicoes-em-inputs.js
+++ Lógica/matando-setTimeOut-para-requisicoes-em-inputs.js
@@ -1,13 +1,13 @@
 // Caso precise fazer requisições a partir de um input de texto, para não precisar fazer uma requisição a cada letra digitada...
 // setamos um timeOut de 1 segundo para esperarmos o usuário parar de digitar...
 
-<input type="text" onChange={searchCid()}></input>
+<input type="text" onChange={search()}></input>
 
 const store = {
   timer: null
 }
 
-const searchCid = () => {
+const search = () => {
   if(store.timer !== null) {
     clearTimeout(this.timer)
   }
